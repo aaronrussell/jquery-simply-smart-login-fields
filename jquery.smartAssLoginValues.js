1690ea20d0086ad2810e3dcb92bbd1ec8ca9019f
Aaron Russell <aaron@gc4.co.uk>
2010-04-01 14:38:56 +0100

    Added copyright notice to plugin source.

--- jquery.smartAssLoginValues.js
+++ jquery.smartAssLoginValues.js
@@ -1,3 +1,16 @@
+/*
+* jQuery Smart Ass Login Values plugin
+* Default values to login forms' username and password fields
+* 
+* @version  0.1
+* @homepage http://github.com/aaronrussell/jquery-smart-ass-login-values/
+* @author   Aaron Russell (http://www.aaronrussell.co.uk)
+*
+* Copyright (c) 2010 Aaron Russell (aaron@gc4.co.uk)
+* Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+* and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+*/
+
 (function($){
   
   $.fn.smartAssLoginValues = function(options){
